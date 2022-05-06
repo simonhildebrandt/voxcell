31a00814352ab17ebc6aea2073fb4b4dc888421a
Simon Hildebrandt <simonhildebrandt@gmail.com>
2022-05-06 14:56:51 +1000

    Cleanup

--- Editor/LevelManagerEditor.cs
+++ Editor/LevelManagerEditor.cs
@@ -30,21 +30,6 @@ class LevelTool : EditorTool {
 
         bool adding = !Event.current.shift;
 
-        // Handles.BeginGUI();
-        // if (adding) {
-        //     if (GUILayout.Button("Remove")) {
-        //         adding = false;
-        //     }
-        // } else {
-        //     if (GUILayout.Button("Add")) {
-        //         adding = true;
-        //     }
-        // }
-        // Handles.EndGUI();
-
-        var evt = Event.current;
-        // if (evt.type == EventType.Repaint) {
-        // }
         float size = .45f;
         float pickSize = size;
 
@@ -69,78 +54,11 @@ class LevelTool : EditorTool {
             }
         }
     }
-
-    // public override void OnDrawGizmos() {
-    //     Gizmos.DrawCube(Vector3.zero, Vector3.one);
-    // }
 }
-
-// public class BuildLevel: EditorWindow {
-//     public static bool editing = false;
-
-//     [MenuItem("Component/Level Tools")]
-//     static void ShowWindow() {
-//         BuildLevel window = ScriptableObject.CreateInstance(typeof(BuildLevel)) as BuildLevel;
-//         window.ShowUtility();
-//     }
-
-//     void OnSceneGUI() {
-//     }
-
-//     void OnGUI() {
-//         GUILayout.Label("Level Editor");
-//         int selected = 0;
-//         string[] options = new string[]
-//         {
-//             "Option1", "Option2", "Option3",
-//         };
-//         selected = EditorGUILayout.Popup("Label", selected, options);
-//     }
-
-//     private static void OnScene(SceneView sceneview) {
-//         Event e = Event.current;
-
-//         if (e.type == EventType.MouseDown && e.button == 0) {
-//             // Debug.Log("here");
-//             Ray worldRay = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
-//             RaycastHit hitInfo;
-//             if (Physics.Raycast(worldRay, out hitInfo)) {
-//                 Vector3 point = hitInfo.transform.InverseTransformPoint(hitInfo.point) * 1.05f;
-//                 Vector3Int direction = Vector3Int.RoundToInt(point);
-//                 Debug.Log(direction);
-
-//                 LevelManager[] lms = FindObjectsOfType<LevelManager>();
-//                 Debug.Log(lms);
-//             }
-
-//             // Steal control to prevent other selection behaviour
-//             // int controlId = GUIUtility.GetControlID(FocusType.Passive);
-//             // GUIUtility.hotControl = controlId;
-//         }
-//     }
-
-//     void OnEnable() {
-//         editing = true;
-//         SceneView.duringSceneGui += OnScene;
-//     }
-
-//     void OnDestroy() {
-//         editing = false;
-//         SceneView.duringSceneGui -= OnScene;
-
-//         // Free selection control
-//         // GUIUtility.hotControl = 0;
-//     }
-// }
-
 
 
 [CustomEditor(typeof(LevelManager))]
-public class LevelManagerEditor : Editor
-{
-//     private static bool m_editMode = false;
-
-
+public class LevelManagerEditor : Editor {
     public override void OnInspectorGUI()
     {
         LevelManager lm = target as LevelManager;
@@ -150,67 +68,14 @@ public class LevelManagerEditor : Editor
 
         if (ToolManager.activeToolType == typeof(LevelTool)) {
             if (blockCount == 0) {
-                if (GUILayout.Button("Add")) {
+                if (GUILayout.Button("Seed")) {
                     lm.AddBlock(Vector3Int.zero);
                 }
             }
-            if (GUILayout.Button("Done")) {
-                ToolManager.RestorePreviousTool();
-            }
         } else {
-            if (GUILayout.Button("EDIT")) {
+            if (GUILayout.Button("Start editing")) {
                 ToolManager.SetActiveTool<LevelTool>();
             }
         }
     }
-
-//         LevelManager level = (LevelManager)target;
-
-//         GameObject source = level.block;
-//         source = (GameObject)EditorGUILayout.ObjectField(source, typeof(GameObject), true);
-//         if (source) {
-//             level.SetBlockPrefab(source);
-//         }
-
-//         if (m_editMode) {
-//             // if (Event.current.type == EventType.MouseDown) {
-//             //     Event.current.Use();
-//             // }
-
-//             // if (Event.current.type == EventType.MouseUp)
-//             // {
-//             //     Debug.Log("here");
-//             //     // Ray worldRay = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
-//             //     // RaycastHit hitInfo;
-
-
-//             //     // if (Physics.Raycast(worldRay, out hitInfo))
-//             //     // {
-//             //     //     GameObject waypoint = Resources.LoadAssetAtPath("Assets/Prefabs/Waypoint.prefab", typeof(GameObject)) as GameObject;
-//             //     //     GameObject waypointInstance = Instantiate(waypoint) as GameObject;
-//             //     //     waypointInstance.transform.position = hitInfo.point;
-//             //     //     waypointInstance.name = "Waypoint" + m_count.ToString("00");
-//             //     //     waypointInstance.transform.parent = m_container.transform;
-//             //     //     Waypoint waypointScript = waypointInstance.GetComponent("Waypoint") as Waypoint;
-//             //     //     waypointScript.id = m_count;
-
-//             //     //     EditorUtility.SetDirty(waypointInstance);
-
-//             //     //     m_count++;
-//             //     // }
-//             //     Event.current.Use();
-//             // }
-
-
-//             if (GUILayout.Button("Finish Editing")) {
-//                 m_editMode = false;
-//             }
-//         } else {
-//             if (GUILayout.Button("Enable Editing")) {
-//                 m_editMode = true;
-
-//                 level.EnsureBlock();
-//             }
-//         }
-//     }
 }
